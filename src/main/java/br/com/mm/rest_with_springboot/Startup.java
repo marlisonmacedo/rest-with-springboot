c0a801a8ddfc4cdf526479b0e4fd1f6a827c957b
marlisonmacedo <marlison.m11@gmail.com>
2026-02-17 22:14:07 -0300

    commit initial

---
+++ src/main/java/br/com/mm/rest_with_springboot/Startup.java
@@ -0,0 +1,13 @@
+package br.com.mm.rest_with_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mm/rest_with_springboot/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.mm.rest_with_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
